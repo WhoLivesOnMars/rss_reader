c47f45035dfb3d0ad73306fff126f7792c857126
Daria KHANINA <khaninadaria@gmail.com>
2026-09-04 13:55:53 +0200

    fix: allow GitHub Pages origin

--- rss_reader_api/config/initializers/cors.rb
+++ rss_reader_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000"
+        origins "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "https://wholivesonmars.github.io"
         resource "*",
             headers: :any,
             methods: [:get, :post, :patch, :put, :delete, :options, :head],
