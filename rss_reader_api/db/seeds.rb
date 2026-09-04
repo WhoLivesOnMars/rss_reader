b505d643d70e405a73679c1bfe40aabddb1cee58
Daria KHANINA <khaninadaria@gmail.com>
2026-09-04 14:05:14 +0200

    feat: add default RSS feeds

--- rss_reader_api/db/seeds.rb
+++ rss_reader_api/db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#   
+feeds = [
+  {
+    title: "BBC News",
+    url: "https://feeds.bbci.co.uk/news/rss.xml"
+  },
+  {
+    title: "NASA",
+    url: "https://www.nasa.gov/feed/"
+  }
+]
+
+feeds.each do |feed|
+  Feed.find_or_create_by!(url: feed[:url]) do |f|
+    f.title = feed[:title]
+  end
+end
+
+puts "Default RSS feeds created."
